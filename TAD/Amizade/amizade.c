8ab91f87821ccaa07cdbee6951733e822e022b74
rafael_torres <souza.t.rafael@gmail.com>
2016-10-28 00:20:54 -0200

    Add Amizade ADT;

--- TAD/Amizade/amizade.c
+++ TAD/Amizade/amizade.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include "amizade.h"
 
 Amizade* iniciaListaAmizade(){
@@ -29,28 +28,77 @@ int amizadeRepetida(Amizade *a, Relacao *r){
   return 0;
 }
 
-void insereAmizade(Amizade *a, int id1, int id2){
+Relacao* retornaRelacao(Amizade *a, int id1, int id2){
+  if(a->qtd > 0){
+    Relacao *r_aux = (Relacao*)malloc(sizeof(Relacao));
+    Relacao *r_retorno = (Relacao*)malloc(sizeof(Relacao));
+
+    r_aux = a->primeira;
+    while (r_aux != NULL) {
+      if((r_aux->id1 == id1 || r_aux->id1 == id2) && (r_aux->id2 == id1 || r_aux->id2 == id2)){
+        r_retorno = r_aux;
+      }
+      r_aux = r_aux->prox;
+    }
+    return r_retorno;
+  }
+}
+
+void ativaAmizade(Relacao *r, int tempo){
+  Momento *m_ultimo = (Momento*)malloc(sizeof(Momento));
+  Momento *m_novo = (Momento*)malloc(sizeof(Momento));
+
+  m_ultimo = r->momento;
+
+  while(m_ultimo->prox != NULL){
+    m_ultimo = m_ultimo->prox;
+  }
+
+  m_ultimo->prox = m_novo;
+
+  m_novo->t_cadastro = tempo;
+  r->ativa = 1;
+}
+
+void insereAmizade(Amizade *a, int id1, int id2, int tempo){
   Relacao *r = (Relacao*)malloc(sizeof(Relacao));
 
   r->id1 = id1;
   r->id2 = id2;
-  r->t_cadastro = time(0);
+  r->ativa = 1;
   r->prox = NULL;
 
-  if(amizadeRepetida(a,r) == 0 && id1 != id2){
-    if(a->qtd <= 0){
-      a->primeira = r;
-      a->ultima = r;
-      a->qtd = 1;
+  if(id1 != id2){
+    if(amizadeRepetida(a,r) == 0){
+      Momento *m = (Momento*)malloc(sizeof(Momento));
 
-      r->ant = NULL;
+      m->prox = NULL;
+      m->t_cadastro = tempo;
+      m->t_cancelamento = -1;
+
+      r->momento = m;
+
+      if(a->qtd <= 0){
+        a->primeira = r;
+        a->ultima = r;
+        a->qtd = 1;
+
+        r->ant = NULL;
+      } else {
+        a->ultima->prox = r;
+
+        r->ant = a->ultima;
+
+        a->ultima = r;
+        a->qtd++;
+      }
     } else {
-      a->ultima->prox = r;
-
-      r->ant = a->ultima;
-
-      a->ultima = r;
-      a->qtd++;
+      r = retornaRelacao(a, id1, id2);
+      if(r != NULL){
+        if(r->ativa == 0){
+          ativaAmizade(r, tempo);
+        }
+      }
     }
   }
 }
--- TAD/Amizade/amizade.h
+++ TAD/Amizade/amizade.h
@@ -1,15 +1,23 @@
-typedef struct a * t_pont;
+typedef struct a * ta_pont;
+
+typedef struct m * tm_pont;
+
+typedef struct m {
+  int t_cadastro, t_cancelamento;
+  tm_pont prox;
+} Momento;
 
 typedef struct a {
-  int id1, id2, t_cadastro;
-  t_pont ant, prox;
+  int id1, id2, ativa;
+  Momento *momento;
+  ta_pont ant, prox;
 } Relacao;
 
 typedef struct h {
   int qtd;
-  t_pont primeira, ultima;
+  ta_pont primeira, ultima;
 } Amizade;
 
 Amizade* iniciaListaAmizade();
 int amizadeRepetida(Amizade *a, Relacao *r);
-void insereAmizade(Amizade *a, int id1, int id2);
+void insereAmizade(Amizade *a, int id1, int id2, int tempo);
