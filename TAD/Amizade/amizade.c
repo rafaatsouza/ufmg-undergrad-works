dcb9a2fbea093c32f09c8fa6b7d2efa01a18975e
rafaatsouza <souza.t.rafael@gmail.com>
2016-11-01 10:14:14 -0200

    Add "ativa" var's check  on verAmigos's function;

--- TAD/Amizade/amizade.c
+++ TAD/Amizade/amizade.c
@@ -127,10 +127,12 @@ Amizade* verAmigos(Amizade *a, int id_user){
 
     r_aux = a->primeira;
     while (r_aux != NULL) {
-      if(r_aux->id1 == id_user){
-          iniciarAmizade(retorno, id_user, r_aux->id2, r_aux->momento->t_cadastro);
-      } else if(r_aux->id2 == id_user) {
-          iniciarAmizade(retorno, id_user, r_aux->id1, r_aux->momento->t_cadastro);
+      if(r_aux->ativa == 1){
+          if(r_aux->id1 == id_user){
+              iniciarAmizade(retorno, id_user, r_aux->id2, r_aux->momento->t_cadastro);
+          } else if(r_aux->id2 == id_user) {
+              iniciarAmizade(retorno, id_user, r_aux->id1, r_aux->momento->t_cadastro);
+          }
       }
       r_aux = r_aux->prox;
     }
