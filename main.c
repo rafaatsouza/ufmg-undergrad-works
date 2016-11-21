d9709293b0fd7623d897a5feab8e9c26f45cc20d
rafaatsouza <souza.t.rafael@gmail.com>
2016-11-21 11:50:05 -0200

    Chance function name;

--- main.c
+++ main.c
@@ -5,5 +5,11 @@
 
 int main(){
     srand((unsigned)time(NULL));
+    // vetor *v = criaVetor(1,10,10);
+    // imprimeVetor(v);
+    // defineOrdenado(v);
+    // ordenaBolha(v);
+    // imprimeVetor(v);
+    // defineOrdenado(v);
     return 0;
 }
