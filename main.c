bc21d47a5b7e9d233003647fc610090eccfae3da
rafael_torres <souza.t.rafael@gmail.com>
2017-04-15 17:58:37 -0300

    Bug fix: > 1 lenght numbers

--- main.c
+++ main.c
@@ -82,18 +82,27 @@ void printResults(filaNumeros *fn){
 }
 
 int main(){
-    int i = 0;
+    int num = 0, i = 0;
     char c;
 
     filaNumeros *fv = createFV();
 
     c = getchar();
     while(c != '\n' && c != '\0'){
-        if(c != ' '){
-            if(c != '?'){
-                addElement(fv, (((int)c)-48), 0);
+        if(c == ' '){
+            if(num > -1){
+                addElement(fv, num, 0);
+            }
+            num = -1;
+        } else if(c == '?') {
+            addElement(fv, 0, 1);
+            num = -1;
+        } else {
+            if(num == -1){
+                num = (((int)c)-48);
             } else {
-                addElement(fv, 0, 1);
+                num *= 10;
+                num += (((int)c)-48);
             }
         }
         c = getchar();
