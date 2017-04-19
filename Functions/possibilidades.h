e2c6a02d989db9bcce99442d588f9d476240dc9f
Rafael Torres Souza <souza.t.rafael@gmail.com>
2017-04-19 09:25:35 -0300

    Update possibilidades.h

--- Functions/possibilidades.h
+++ Functions/possibilidades.h
@@ -1,3 +1,1 @@
-void printaPossibilidade(char *possibilidade, int operadores);
-void converteParaDecimal(long long int num, char *possibilidade, int operadores);
 void exibeResultados(filaElementos *fn);
