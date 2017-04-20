733ba3e5ecaf09f1285c3648e6d529f4b2512e13
Rafael Torres Souza <souza.t.rafael@gmail.com>
2017-04-20 13:55:50 -0300

    Update possibilidades.c
    Adds possibilidades.c commentaries

--- Functions/possibilidades.c
+++ Functions/possibilidades.c
@@ -5,6 +5,7 @@
 #include "../TAD/pilhaOperacoes.h"
 #include "possibilidades.h"
 
+//recebe o vetor de char's referente à sequência de 0's e 1's e printa na tela a sequência de operadores equivalente
 void printaPossibilidade(char *possibilidade, int operadores){
     int i;
     for(i=0;i<operadores;i++){
@@ -17,6 +18,7 @@ void printaPossibilidade(char *possibilidade, int operadores){
     printf("\n");
 }
 
+//recebe um numero e converte de decimal para binario, alocando o resultado num vetor de char's que tem tamanho = int operadores
 void converteParaBinario(long long int num, char *possibilidade, int operadores){
     long long int j, i = operadores-1, resto;
     while(num >= 1){
@@ -34,23 +36,25 @@ void converteParaBinario(long long int num, char *possibilidade, int operadores)
             break;
         }
     }
+    //completa o resto do vetor com 0's. Ex: conv 3 para binario = 11. Caso a máscara seja de 6 operadores, o vetor fica "000011"
     for(j=i;j>=0;j--){
         possibilidade[j] = '0';
     }
 }
 
+//recebe a fila de elementos informada, seu resultado, e printa todas as possbilidades de operadores possíveis
 void exibeResultados(filaElementos *fn){
-    unsigned long long int i, count = pow(2,fn->count_operadores);
+    unsigned long long int i, count = pow(2,fn->count_operadores); //count é a quantidade de possibilidades, definida por 2^(quantidade de "?")
     int j;
 
-    char *possibilidadeAtual = (char*)malloc(sizeof(char)*fn->count_operadores);
+    char *possibilidadeAtual = (char*)malloc(sizeof(char)*fn->count_operadores); //cria vetor de char's para máscara de operadores
 
     for(i=0;i<count;i++){
-        converteParaBinario(i,possibilidadeAtual, fn->count_operadores);
-        pilhaOperacoes *pilha = createPV();
+        converteParaBinario(i,possibilidadeAtual, fn->count_operadores); //converte o contador do for para binário, para ser usado como máscara de operadores
+        pilhaOperacoes *pilha = createPV(); //cria uma pilha vazia
         j = 0;
         Value *actual = fn->first;
-        while(actual != NULL && (pilha->topo == NULL || pilha->topo->val <= fn->resultado)){
+        while(actual != NULL && (pilha->topo == NULL || pilha->topo->val <= fn->resultado)){ //passa por todos os elementos da fila, usando a pilha para aplicar os operadores da tentativa atual à sequência de númros
             if(actual->iOperador == 1){
                 if(possibilidadeAtual[j] == '0'){
                     fazOperacao(pilha,1);
@@ -64,9 +68,9 @@ void exibeResultados(filaElementos *fn){
             actual = actual->next;
         }
         if(pilha->result == fn->resultado){
-            printaPossibilidade(possibilidadeAtual, fn->count_operadores);
+            printaPossibilidade(possibilidadeAtual, fn->count_operadores); //caso o resultado final da pilha seja = informado na entrada, printa a sequência de operadores testada
         }
-        freePilha(pilha);
+        freePilha(pilha); //desaloca a memória referente à pilha
     }
-    free(possibilidadeAtual);
+    free(possibilidadeAtual); //desaloca memória do vetor de char's usado para máscara de operadores
 }
