932b6e8ee94fb7b92b3e95069d80b0e76cd896c5
rafael_torres <souza.t.rafael@gmail.com>
2017-05-20 13:48:16 -0300

    Fix memory read error

--- fila.c
+++ fila.c
@@ -3,7 +3,7 @@
 
 filaNumeros* criaFilaVazia(int maxTam){
     filaNumeros *fv = (filaNumeros*)malloc(sizeof(filaNumeros));
-    fv->v = (int*)malloc(sizeof(int));
+    fv->v = (int*)malloc(sizeof(int) * (maxTam));
     fv->qtdNumeros = 0;
 }
 
@@ -16,16 +16,18 @@ int filaVazia(filaNumeros *fv){
 }
 
 void enfileira(filaNumeros *fv, int v){
-    fv->v[fv->qtdNumeros++] = v;
+    fv->v[fv->qtdNumeros] = v;
+    fv->qtdNumeros = fv->qtdNumeros + 1;
 }
+
 int desenfileira(filaNumeros *fv){
     int i;
     if(filaVazia(fv) != 1){
         int primeiro = fv->v[0];
-        for(i=0;i<fv->qtdNumeros-1;i++){
+        for(i=0;i<(fv->qtdNumeros)-1;i++){
             fv->v[i] = fv->v[i+1];
         }
-        fv->qtdNumeros--;
+        fv->qtdNumeros = fv->qtdNumeros - 1;
         return primeiro;
     } else {
         fprintf(stderr, "Erro - nao eh possivel desenfileirar de uma fila vazia.\n");
