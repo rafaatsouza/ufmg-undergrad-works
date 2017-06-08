51c5e2079cb4e876c7ce6927ddd6d08828aaef94
Rafael Torres Souza <rafaeltsouza@vermelho.grad.dcc.ufmg.br>
2017-06-07 21:24:14 -0300

    New method; right output file, wrong order

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void OrdenaIndices(FILE *arquivo, char *diretorio, int qtdLinhas, int qtdMemoria){
+    int i, qtdFitas = (qtdLinhas * 32)/qtdMemoria;
+    
+    if((qtdLinhas * 32)%qtdMemoria) { qtdFitas++; }
+
+    qtdFitas++;
+
+    printf("%d linhas, %d fitas\n", qtdLinhas, qtdFitas);
+}
 
 int main(int argc, char *argv[]){
-    int qtdDocumentos, qtdMemoria, i;
-    char dirEntrada[300], dirSaida[300], nomArqSaida[300], nomArqLeitura[300], *buf;
+    int qtdDocumentos, qtdMemoria, contadorLetra, contadorPalavra, qtdLinhas = 0, i;
+    char dirEntrada[255], dirSaida[255], nomArqSaida[255], nomArqLeitura[255], palavra[20], *buf;
     FILE *saida, *leitura;
 
     scanf("%d %d", &qtdDocumentos, &qtdMemoria);
@@ -18,13 +29,20 @@ int main(int argc, char *argv[]){
     saida = fopen(nomArqSaida,"a");
 
     for(i=1;i<=qtdDocumentos;i++){
-        buf = (char*)malloc(sizeof(char)*qtdMemoria);
+        contadorLetra = 0;
+        contadorPalavra = 1;
         sprintf(nomArqLeitura, "%s%d", dirEntrada, i);
         leitura = fopen(nomArqLeitura,"r");
-        while (fgets(buf,qtdMemoria,leitura) != NULL) {
-            fprintf(saida, "%s\n", strtok(buf,""));
+        while(!feof(leitura)){
+            fscanf(leitura,"%s",palavra);
+            palavra[strlen(palavra)] = '\0';
+            fprintf(saida, "%s,%d,%d,%d\n", palavra, i, contadorPalavra, contadorLetra);
+            qtdLinhas++;
+            contadorLetra += strlen(palavra);
         }
         fclose(leitura);
     }
     fclose(saida);
+
+    OrdenaIndices(saida, nomArqSaida, qtdLinhas, qtdMemoria);
 }
