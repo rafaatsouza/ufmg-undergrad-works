f7d732dcd15ac55604b6d5c8bc6b0201deb0e946
rafael_torres <souza.t.rafael@gmail.com>
2017-06-30 13:13:01 -0300

    Adds 'force algorithm' option

--- main.c
+++ main.c
@@ -7,17 +7,32 @@ int main(int argc, char *argv[]){
     clock_t tempo_exec;
     char tipoAlgoritmo;
     vizinhanca *v;
-    int qtdBar, mostraTempo = 0;
+    int qtdBar, mostraTempo = 0, forcaTipoAlgoritmo = 0;
 
     if(argc >= 2){
         if(strcmp(argv[1],"-t") == 0){
             mostraTempo = 1;
+        } else if(strcmp(argv[1],"-d") == 0){
+            forcaTipoAlgoritmo = 1;
+        } else if(strcmp(argv[1],"-g") == 0){
+            forcaTipoAlgoritmo = 2;
+        } else if(strcmp(argv[1],"-b") == 0){
+            forcaTipoAlgoritmo = 3;
+        }
+        if(argc >= 3){
+            if(strcmp(argv[2],"-d") == 0){
+                forcaTipoAlgoritmo = 1;
+            } else if(strcmp(argv[2],"-g") == 0){
+                forcaTipoAlgoritmo = 2;
+            } else if(strcmp(argv[2],"-b") == 0){
+                forcaTipoAlgoritmo = 3;
+            }
         }
     }
 
     tipoAlgoritmo = getchar();
 
-    if(tipoAlgoritmo != 'd' && tipoAlgoritmo != 'g' && tipoAlgoritmo != 'b'){
+    if(tipoAlgoritmo != 'd' && tipoAlgoritmo != 'g' && tipoAlgoritmo != 'b' && forcaTipoAlgoritmo == 0){
         fprintf(stderr, "Error - Tipo de algoritmo nao foi encontrado\n");
         exit(-1);
     }
@@ -30,12 +45,22 @@ int main(int argc, char *argv[]){
 
     if(mostraTempo == 1){ tempo_exec = clock(); }
 
-    if(tipoAlgoritmo == 'd'){
-        dinamica(v);
-    } else if(tipoAlgoritmo == 'g'){
-        guloso(v);
-    } else if(tipoAlgoritmo == 'b'){
-        bruta(v);
+    if(forcaTipoAlgoritmo == 0){
+        if(tipoAlgoritmo == 'd'){
+            dinamica(v);
+        } else if(tipoAlgoritmo == 'g'){
+            guloso(v);
+        } else if(tipoAlgoritmo == 'b'){
+            bruta(v);
+        }
+    } else {
+        if(forcaTipoAlgoritmo == 1){
+            dinamica(v);
+        } else if(forcaTipoAlgoritmo == 2){
+            guloso(v);
+        } else if(forcaTipoAlgoritmo == 3){
+            bruta(v);
+        }
     }
 
     if(mostraTempo == 1){
