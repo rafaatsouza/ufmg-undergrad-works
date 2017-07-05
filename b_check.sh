e0cf69b3308f8fcdbeaaab699af3deea45706fe1
rafael_torres <souza.t.rafael@gmail.com>
2017-07-05 19:12:23 -0300

    Alter tests

--- b_check.sh
+++ b_check.sh
@@ -3,7 +3,7 @@
 i=0
 make
 while [ "$i" -le 9 ]; do
-    ./tp3.out -b < toys/entrada/toy_"$i" > saida.txt
+    ./tp3.out < toys/entrada_bruta/toy_"$i" > saida.txt
     diff -q toys/saida/toy_"$i" saida.txt
     rm -f saida.txt
     echo "Passou o $i"
--- g_check.sh
+++ g_check.sh
@@ -3,7 +3,7 @@
 i=0
 make
 while [ "$i" -le 9 ]; do
-    ./tp3.out -g < toys/entrada/toy_"$i" > saida.txt
+    ./tp3.out < toys/entrada_gulosa/toy_"$i" > saida.txt
     diff -q toys/saida/toy_"$i" saida.txt
     rm -f saida.txt
     echo "Passou o $i"
