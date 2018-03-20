733e75ed400bb028af0e81e6061ff12979e0607b
rafaatsouza <souza.t.rafael@gmail.com>
2018-03-19 23:17:18 -0300

    Adds firts cpp main file

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+  if(argc < 3){
+    std::cout << "Error - Invalid arguments" << '\n';
+    exit(EXIT_FAILURE);
+  }
+
+  char ratingsFileName[20];
+  char targetsFileName[20];
+
+  strcpy(ratingsFileName, argv[1]);
+  strcpy(targetsFileName, argv[2]);
+
+  std::cout << ratingsFileName << '\n';
+  std::cout << targetsFileName << '\n';
+
+  return 0;
+}
