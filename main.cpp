2527c26c0c27505bb776f71f7b1cde9f99f3f45b
Rafael Torres Souza <rafaeltsouza@paraopeba.grad.dcc.ufmg.br>
2018-03-28 14:54:40 -0300

    Sets c split

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <string.h>
 #include <vector>
 #include <stdlib.h>
@@ -12,18 +14,23 @@ int main(int argc, char *argv[]){
   }
 
   char line[1024];
-  char* content;
+  char *content, *identifier, *prediction, *itemId, *userId;
 
-  FILE* stream = fopen(argv[1], "r"); //ratingsFileName
+  FILE* stream = fopen(argv[1], "r"); //argv[1] = ratingsFileName
   while (fgets(line, 1024, stream))
-    {
-        if (line[0] != '\n' && line[0] != ' '){
-          content = strtok(line,"");
-          printf("%s\n", content);
-        }
-    }
-    
-  //std::cout << "ID: " << movieList.size() << '\n';
+  {
+      if (line[0] != '\n' && line[0] != ' '){
+        content = strtok(line,"");
+        
+        identifier = strtok(content,",");
+        prediction = strtok(NULL,",");
+
+        userId = strtok(identifier,":");
+        itemId = strtok(NULL,":");
+
+        std::cout << "Usuario " << userId << " - Item " << itemId << " - Prediction " << prediction << '\n';
+      }
+  }
 
   return 0;
 }
