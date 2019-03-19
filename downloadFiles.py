680f9cd1464fe6b126a093cd4aece924dca17e58
Rafael Torres <souza.t.rafael@gmail.com>
2019-03-18 23:06:00 -0300

    Adds try-catch and duplicate image check

---
+++ downloadFiles.py
@@ -0,0 +1,23 @@
+import urllib.request
+import pandas as pd
+import os
+
+csvFileName = 'files.csv'
+folderName = 'images/uncategorized'
+
+if not os.path.exists(folderName):
+    os.makedirs(folderName)
+
+df = pd.read_csv(csvFileName, sep=";")
+imagesNameList = []
+
+for index, row in df.iterrows():
+    filename = 'image_' + str(index) + '.' + (row['name'].split("."))[1]
+    try:
+        if(row['name'] not in imagesNameList):
+            urllib.request.urlretrieve(row['link'], folderName + '/' + filename)
+            imagesNameList.append(row['name'])
+        else:
+            print('what')
+    except:
+        print('Error with {}'.format(filename))
